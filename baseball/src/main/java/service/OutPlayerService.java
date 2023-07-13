5984f537f8e0d3c4264d2a4b128b6482fadeaea0
wlstjdgns <62293275+wlstjdgns@users.noreply.github.com>
2023-07-13 09:43:08 +0900

    Delete baseball directory

--- baseball/src/main/java/service/OutPlayerService.java
+++
@@ -1,9 +0,0 @@
-package service;
-import java.sql.Connection;
-
-public class OutPlayerService {
-    private Connection connection;
-    public OutPlayerService(Connection connection) {
-        this.connection = connection;
-    }
-}
